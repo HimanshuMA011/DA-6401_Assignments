2b3847fe769497a28c3fdca315c7be375c6b56ca
Himanshu Jangir <140971096+HimanshuMA011@users.noreply.github.com>
2025-03-09 19:33:17 +0530

    Update neural_network.py

--- neural_network.py
+++ neural_network.py
@@ -7,17 +7,17 @@ from imp_function import *
 
 
 class FeedForwardNeuralNetwork:
-    def __init__(self, hidden_layers, no_of_hidden_neurons, X_train, Y_train, N_train,
-                  X_val, Y_val, N_val,X_test,Y_test_raw, N_test, optimizer,batch_size, 
+    def __init__(self, hidden_layers, no_of_hidden_neurons, train_In, train_out, N_train,
+                  X_val_In, val_out, N_val,test_In,test_out, N_test, optimizer,batch_size, 
                   weight_decay,learning_rate, max_epochs,activation, initializer,loss):
 
         
-        self.num_classes = np.max(Y_train) + 1  # NUM_CLASSES
+        self.num_classes = np.max(train_out) + 1  # NUM_CLASSES
         self.hidden_layers = hidden_layers
         self.no_of_hidden_neurons = no_of_hidden_neurons
         self.output_layer_size = self.num_classes
-        self.img_height = X_train.shape[1]
-        self.img_width = X_train.shape[2]
+        self.img_height = train_In.shape[1]
+        self.img_width = train_In.shape[2]
         self.img_flattened_size = self.img_height * self.img_width
        
         self.layers = (
@@ -32,30 +32,30 @@ class FeedForwardNeuralNetwork:
         
 
         # to convert image pixels to row vectors
-        self.X_train = np.transpose(
-            X_train.reshape(
-                X_train.shape[0], X_train.shape[1] * X_train.shape[2]
+        self.train_In = np.transpose(
+            train_In.reshape(
+                train_In.shape[0], train_In.shape[1] * train_In.shape[2]
             )
         ) 
-        self.X_test = np.transpose(
-            X_test.reshape(
-                X_test.shape[0], X_test.shape[1] * X_test.shape[2]
+        self.test_In = np.transpose(
+            test_In.reshape(
+                test_In.shape[0], test_In.shape[1] * test_In.shape[2]
             )
         )  
-        self.X_val = np.transpose(
-            X_val.reshape(
-                X_val.shape[0], X_val.shape[1] * X_val.shape[2]
+        self.X_val_In = np.transpose(
+            X_val_In.reshape(
+                X_val_In.shape[0], X_val_In.shape[1] * X_val_In.shape[2]
             )
         )
 
         ## to convert color scale to 0-1 range from 0-255
-        self.X_train = self.X_train / 255
-        self.X_test = self.X_test / 255
-        self.X_val = self.X_val / 255
+        self.train_In = self.train_In / 255
+        self.test_In = self.test_In / 255
+        self.X_val_In = self.X_val_In / 255
         
-        self.Y_train = self.oneHotEncode(Y_train)  # [NUM_CLASSES X NTRAIN]
-        self.Y_val = self.oneHotEncode(Y_val)
-        self.Y_test = self.oneHotEncode(Y_test_raw)
+        self.train_out = self.oneHotEncode(train_out)  # [NUM_CLASSES X NTRAIN]
+        self.val_out = self.oneHotEncode(val_out)
+        self.Y_test = self.oneHotEncode(test_out)
 
 
         self.all_Activations = {"sigmoid": sigmoid, "tanh": tanh, "ReLU": relu}
@@ -63,7 +63,7 @@ class FeedForwardNeuralNetwork:
 
         self.all_Initializer = {"XAVIER": self.Xavier_initializer, "RANDOM": self.random_initializer}
 
-        self.Optimizer_dict = {"SGD": self.sgdMiniBatch,"MGD": self.mgd,"NAG": self.nag,
+        self.Optimizer_dict = {"SGD": self.sgdMB,"MGD": self.mgd,"NAG": self.nag,
                                "RMSPROP": self.rmsProp,"ADAM": self.adam,"NADAM": self.nadam}
         
         self.activation = self.all_Activations[activation]
@@ -81,10 +81,10 @@ class FeedForwardNeuralNetwork:
         
         
     # helper functions
-    def oneHotEncode(self, Y_train):
-        Ydata = np.zeros((self.num_classes, Y_train.shape[0]))
-        for i in range(Y_train.shape[0]):
-            value = Y_train[i]
+    def oneHotEncode(self, train_out):
+        Ydata = np.zeros((self.num_classes, train_out.shape[0]))
+        for i in range(train_out.shape[0]):
+            value = train_out[i]
             Ydata[int(value)][i] = 1.0
         return Ydata
 
@@ -98,7 +98,7 @@ class FeedForwardNeuralNetwork:
         crossEntropy = np.mean(CE)
         return crossEntropy
 
-    def L2RegularisationLoss(self, weight_decay):
+    def L2_reg_loss(self, weight_decay):
         ALPHA = weight_decay
         return ALPHA * np.sum(
             [
@@ -135,32 +135,32 @@ class FeedForwardNeuralNetwork:
     def initializeNeuralNet(self, layers):
         weights = {}
         biases = {}
-        num_layers = len(layers)
-        for l in range(0, num_layers - 1):
+        no_of_layers = len(layers)
+        for l in range(0, no_of_layers - 1):
             W = self.initializer(size=[layers[l + 1], layers[l]])
             b = np.zeros((layers[l + 1], 1))
             weights[str(l + 1)] = W
             biases[str(l + 1)] = b
         return weights, biases
 
-    def forwardPropagate(self, X_train_batch, weights, biases):
+    def forwardPropagate(self, train_In_batch, weights, biases):
         """
         Returns the neural network given input data, weights, biases.
         Arguments:
-                 : X_train_batch - input matrix
+                 : train_In_batch - input matrix
                  : Weights  - Weights matrix
                  : biases - Bias vectors 
         """
         # Number of layers = length of weight matrix + 1
-        num_layers = len(weights) + 1
+        no_of_layers = len(weights) + 1
         # A - Preactivations
         # H - Activations
-        X = X_train_batch
+        X = train_In_batch
         H = {}
         A = {}
         H["0"] = X
         A["0"] = X
-        for l in range(0, num_layers - 2):
+        for l in range(0, no_of_layers - 2):
             if l == 0:
                 W = weights[str(l + 1)]
                 b = biases[str(l + 1)]
@@ -173,19 +173,19 @@ class FeedForwardNeuralNetwork:
                 H[str(l + 1)] = self.activation(A[str(l + 1)])
 
         # Here the last layer is not activated
-        W = weights[str(num_layers - 1)]
-        b = biases[str(num_layers - 1)]
-        A[str(num_layers - 1)] = np.add(np.matmul(W, H[str(num_layers - 2)]), b)
+        W = weights[str(no_of_layers - 1)]
+        b = biases[str(no_of_layers - 1)]
+        A[str(no_of_layers - 1)] = np.add(np.matmul(W, H[str(no_of_layers - 2)]), b)
         # Y = softmax(A[-1])
-        Y = softmax(A[str(num_layers - 1)])
-        H[str(num_layers - 1)] = Y
+        Y = softmax(A[str(no_of_layers - 1)])
+        H[str(no_of_layers - 1)] = Y
         return Y, H, A
 
-    def backPropagate(self, Y, H, A, Y_train_batch, weight_decay=0):
+    def backPropagate(self, Y, H, A, train_out_batch, weight_decay=0):
         ALPHA = weight_decay
         gradients_weights = []
         gradients_biases = []
-        num_layers = len(self.layers)
+        no_of_layers = len(self.layers)
 
         dA = {}  # Store activation gradients
         dH = {}  # Store hidden gradients
@@ -194,11 +194,11 @@ class FeedForwardNeuralNetwork:
 
         # Compute gradient for the output layer
         if self.loss_function == "CROSS":
-            dA[num_layers - 1] = -(Y_train_batch - Y)
+            dA[no_of_layers - 1] = -(train_out_batch - Y)
         elif self.loss_function == "MSE":
-            dA[num_layers - 1] = np.multiply(2 * (Y - Y_train_batch), np.multiply(Y, (1 - Y)))
+            dA[no_of_layers - 1] = np.multiply(2 * (Y - train_out_batch), np.multiply(Y, (1 - Y)))
 
-        for l in range(num_layers - 2, -1, -1):
+        for l in range(no_of_layers - 2, -1, -1):
             dW[l + 1] = np.outer(dA[l + 1], H[str(l)]) + (ALPHA * self.weights[str(l + 1)] if ALPHA != 0 else 0)
             dB[l + 1] = dA[l + 1]
 
@@ -215,9 +215,9 @@ class FeedForwardNeuralNetwork:
         return gradients_weights, gradients_biases
 
 
-    def predict(self,X,length_dataset):
+    def predict(self,X,data_length):
         Y_pred = []        
-        for i in range(length_dataset):
+        for i in range(data_length):
 
             Y, H, A = self.forwardPropagate(
                 X[:, i].reshape(self.img_flattened_size, 1),
@@ -229,3 +229,186 @@ class FeedForwardNeuralNetwork:
         Y_pred = np.array(Y_pred).transpose()
         return Y_pred
 
+    #Optimisers defined here onwards
+    def sgd(self, epochs, data_length, learning_rate, weight_decay=0):
+        
+        training_loss = []
+        training_accuracy = []
+        validation_accuracy = []
+        
+        no_of_layers = len(self.layers)
+
+        train_In = self.train_In[:, :data_length]
+        train_out = self.train_out[:, :data_length]
+
+        for epoch in range(epochs):
+            start_time = time.time()
+            
+            idx = np.random.shuffle(np.arange(data_length))
+            train_In = train_In[:, idx].reshape(self.img_flattened_size, data_length)
+            train_out = train_out[:, idx].reshape(self.num_classes, data_length)
+            
+            LOSS = []
+
+            
+            deltaw = [
+                np.zeros((self.layers[l + 1], self.layers[l]))
+                for l in range(0, len(self.layers) - 1)
+            ]
+            deltab = [
+                np.zeros((self.layers[l + 1], 1))
+                for l in range(0, len(self.layers) - 1)
+            ]
+
+            for i in range(data_length):
+
+                Y, H, A = self.forwardPropagate(
+                    train_In[:, i].reshape(self.img_flattened_size, 1),
+                    self.weights,
+                    self.biases,
+                )
+                dWeights, dBiases = self.backPropagate(
+                    Y, H, A, train_out[:, i].reshape(self.num_classes, 1)
+                )
+                deltaw = [
+                    dWeights[no_of_layers - 2 - i] for i in range(no_of_layers - 1)
+                ]
+                deltab = [
+                    dBiases[no_of_layers - 2 - i] for i in range(no_of_layers - 1)
+                ]
+
+
+                if self.loss_function == "MSE":
+                    LOSS.append(self.meanSquaredErrorLoss(
+                            self.train_out[:, i].reshape(self.num_classes, 1), Y
+                        )
+                        + self.L2_reg_loss(weight_decay)
+                        )
+                elif self.loss_function == "CROSS":
+                    LOSS.append(
+                        self.crossEntropyLoss(
+                            self.train_out[:, i].reshape(self.num_classes, 1), Y
+                        )
+                        + self.L2_reg_loss(weight_decay)
+                    )
+
+                
+                self.weights = {
+                    str(i + 1): (self.weights[str(i + 1)] - learning_rate * deltaw[i])
+                    for i in range(len(self.weights))
+                }
+                self.biases = {
+                    str(i + 1): (self.biases[str(i + 1)] - learning_rate * deltab[i])
+                    for i in range(len(self.biases))
+                }
+
+            elapsed = time.time() - start_time
+            
+            Y_pred = self.predict(self.train_In, self.N_train)
+            
+            training_loss.append(np.mean(LOSS))
+            training_accuracy.append(self.accuracy(train_out, Y_pred, data_length)[0])
+            validation_accuracy.append(self.accuracy(self.val_out, self.predict(self.X_val_In, self.N_val), self.N_val)[0])
+            
+            print(
+                        "Epoch: %d, Loss: %.3e, Training accuracy:%.3f, Validation Accuracy: %.3f, Time: %.3f, Learning Rate: %.3e"
+                        % (
+                            epoch,
+                            training_loss[epoch],
+                            training_accuracy[epoch],
+                            validation_accuracy[epoch],
+                            elapsed,
+                            self.learning_rate,
+                        )
+                    )
+
+            wandb.log({'loss':np.mean(LOSS), 'training_accuracy':training_accuracy[epoch], 'validation_accuracy':validation_accuracy[epoch],'epoch':epoch, })
+
+        return training_loss, training_accuracy, validation_accuracy, Y_pred
+
+
+      
+    def sgdMB(self, epochs,data_length, batch_size, learning_rate, weight_decay = 0):
+
+        train_In = self.train_In[:, :data_length]
+        train_out = self.train_out[:, :data_length]        
+
+        training_loss = []
+        training_accuracy = []
+        validation_accuracy = []
+        
+        no_of_layers = len(self.layers)
+        num_to_update = 0
+
+
+        for epoch in range(epochs):
+            start_time = time.time()
+            idx = np.random.shuffle(np.arange(data_length))
+            train_In = train_In[:, idx].reshape(self.img_flattened_size, data_length)
+            train_out = train_out[:, idx].reshape(self.num_classes, data_length)
+            
+            LOSS = []
+            #Y_pred = []
+            
+            deltaw = [np.zeros((self.layers[l+1], self.layers[l])) for l in range(0, len(self.layers)-1)]
+            deltab = [np.zeros((self.layers[l+1], 1)) for l in range(0, len(self.layers)-1)]
+
+            for i in range(data_length):
+                
+                Y,H,A = self.forwardPropagate(train_In[:,i].reshape(self.img_flattened_size,1), self.weights, self.biases) 
+                dWeights, dBiases = self.backPropagate(Y,H,A,train_out[:,i].reshape(self.num_classes,1))
+                
+                deltaw = [dWeights[no_of_layers-2 - i] + deltaw[i] for i in range(no_of_layers - 1)]
+                deltab = [dBiases[no_of_layers-2 - i] + deltab[i] for i in range(no_of_layers - 1)]
+                
+                if self.loss_function == "MSE":
+                    LOSS.append(self.meanSquaredErrorLoss(
+                            self.train_out[:, i].reshape(self.num_classes, 1), Y
+                        )
+                        + self.L2_reg_loss(weight_decay)
+                        )
+                elif self.loss_function == "CROSS":
+                    LOSS.append(
+                        self.crossEntropyLoss(
+                            self.train_out[:, i].reshape(self.num_classes, 1), Y
+                        )
+                        + self.L2_reg_loss(weight_decay)
+                    )
+                num_to_update +=1
+                
+                if int(num_to_update) % batch_size == 0:
+                    
+                    
+                    self.weights = {str(i+1):(self.weights[str(i+1)] - learning_rate*deltaw[i]/batch_size) for i in range(len(self.weights))} 
+                    self.biases = {str(i+1):(self.biases[str(i+1)] - learning_rate*deltab[i]) for i in range(len(self.biases))}
+                    
+                    #resetting gradient updates
+                    deltaw = [np.zeros((self.layers[l+1], self.layers[l])) for l in range(0, len(self.layers)-1)]
+                    deltab = [np.zeros((self.layers[l+1], 1)) for l in range(0, len(self.layers)-1)]
+            
+            elapsed = time.time() - start_time
+
+            Y_pred = self.predict(self.train_In, self.N_train)
+            training_loss.append(np.mean(LOSS))
+            training_accuracy.append(self.accuracy(train_out, Y_pred, data_length)[0])
+            validation_accuracy.append(self.accuracy(self.val_out, self.predict(self.X_val_In, self.N_val), self.N_val)[0])
+
+            print(
+                        "Epoch: %d, Loss: %.3e, Training accuracy:%.3f, Validation Accuracy: %.3f, Time: %.3f, Learning Rate: %.3e"
+                        % (
+                            epoch,
+                            training_loss[epoch],
+                            training_accuracy[epoch],
+                            validation_accuracy[epoch],
+                            elapsed,
+                            self.learning_rate,
+                        )
+                    )
+                    
+            wandb.log({'loss':np.mean(LOSS), 'training_accuracy':training_accuracy[epoch], 'validation_accuracy':validation_accuracy[epoch],'epoch':epoch })
+            
+        return training_loss, training_accuracy, validation_accuracy, Y_pred
+
+
+
+    
